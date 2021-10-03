e818f5ff48f89264f0c83db2951bf1b9a3b0c7c7
sonal765 <sonalj7657@gmail.com>
2021-10-03 20:13:03 +0530

    add chat feature

--- lib/user.dart
+++ lib/user.dart
@@ -1,19 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/login_screen.dart';
 
-Future<User?> createAccount(String name, String email, String password) async {
+Future<User> createAccount(String name, String email, String password) async {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
   try {
-    User? user = (await _auth.createUserWithEmailAndPassword(
+    User user = (await _auth.createUserWithEmailAndPassword(
             email: email, password: password))
         .user;
 
     if (user != null) {
       print("Account created Succesfull");
+      // user.updateDisplayName(displayName: name);
+      user.updateDisplayName(name);
+      // user.updateProfile(displayName: name);
+      await _firestore.collection('users').doc(_auth.currentUser.uid).set({
+        "name": name,
+        "email": email,
+        "status": "Unavailabe",
+      });
       return user;
     } else {
       print("Account creation failed");
@@ -25,10 +35,10 @@ Future<User?> createAccount(String name, String email, String password) async {
   }
 }
 
-Future<User?> logIn(String email, String password) async {
+Future<User> logIn(String email, String password) async {
   FirebaseAuth _auth = FirebaseAuth.instance;
   try {
-    User? user = (await _auth.signInWithEmailAndPassword(
+    User user = (await _auth.signInWithEmailAndPassword(
             email: email, password: password))
         .user;
 
